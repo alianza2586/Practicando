3cdef723560a85af1e418b4378afe59583eaa2ea
Wilson <122341774+alianza2586@users.noreply.github.com>
2024-07-08 21:25:44 -0700

    Update ejercicio-1.js
    Uso de arreglos en classes

--- ejercicio-1.js
+++ ejercicio-1.js
@@ -1,9 +1,34 @@
-/* 
-Practicando ejercicios
- roberto = 1.5;
- pedro = 1.7;
- cofla = 3;
-*/
+class Trabajador{
+    constructor(nombre,apellido,edad){
+        this.nombre = nombre;
+        this.apellido = apellido;
+        this.edad = edad;
+    }
+    infoTrabajador(){
+        return  `Nombre : ${this.nombre} - Apellido: ${this.apellido} - Edad: ${this.edad} años`;
+    }
 
-let monto = prompt("Ingrese el monto :");
+}
 
+class Empleador{
+    constructor(){
+        this.empleado=[];
+    }
+    agregarTrabajador(empleado){
+         this.empleado.push(empleado);
+    }
+    
+}
+let trabajador1 = new Trabajador("Andre","Saavedra",37);
+let trabajador2 = new Trabajador("Miguel","Diaz",34);
+let trabajador3 = new Trabajador("Jose","Urbano",25);
+
+let persona = new Empleador();
+
+persona.agregarTrabajador(trabajador1);
+persona.agregarTrabajador(trabajador2);
+persona.agregarTrabajador(trabajador3);
+
+console.log(trabajador1.infoTrabajador());
+console.log(trabajador2.infoTrabajador());
+console.log(trabajador3.infoTrabajador());
