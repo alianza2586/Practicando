750ad7a529513d106e8f5e43a9377eee25a3b365
Wilson <saavedraurbano@gmail.com>
2025-06-09 20:35:50 -0700

    mensaje desktop

--- ejercicio-2.js
+++ ejercicio-2.js
@@ -6,7 +6,7 @@ De una lista de segundos, conviértelos a minutos redondeando a 2 decimales.
 const segundos = [90, 120, 75];
 // Resultado esperado: [1.5, 2.0, 1.25]
 
-// agregando en github
+// agregando en escritorio
 
 let minu = segundos.map(sg => sg / 60);
 console.log(minu);
